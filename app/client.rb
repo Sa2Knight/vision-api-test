553e084a8581d1e35ee4880a990460756bc073fe
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-06-10 08:52:44 +0000

    APIに対してPOSTするメソッドを実装

--- app/client.rb
+++ app/client.rb
@@ -1,13 +1,29 @@
 require 'net/http'
+require 'json'
 
 class Client
 
+  @@API_KEY = ENV['GOOGLEAPI']
+  @@API_URL = "https://vision.googleapis.com/v1/images:annotate?key=#{@@API_KEY}"
+
   def initialize(params)
-    @params = params
+    @params  = params
   end
 
   def get_result
-    'result'
+    post()
+  end
+
+  private
+  def post(params = {})
+    uri  = URI.parse(@@API_URL)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req["Content-Type"] = "application/json"
+    req.body = JSON.generate(params)
+    res = http.request(req)
+    res.body
   end
 
 end
